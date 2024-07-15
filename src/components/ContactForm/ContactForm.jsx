6f95e3e85b1a55a22216497898d66b2e44d2570f
Olena000 <154604010+Olena000@users.noreply.github.com>
2024-07-15 23:54:27 +0300

    2/2

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,5 +1,6 @@
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import s from "./ContactForm.module.css";
+import * as Yup from "yup";
 
 export default function ContactForm() {
   const initialValues = {
@@ -7,17 +8,33 @@ export default function ContactForm() {
     number: "",
   };
 
+  const registerSchema = Yup.object({
+    name: Yup.string()
+      .required("This field is required")
+      .min(3, "Name must be more than 3 chars!")
+      .max(50, "Name must be less than 50 chars!"),
+  });
+
   const handleSubmit = (data, actions) => {
     console.log(data);
     actions.resetForm();
   };
   return (
     <div className={s.formWrapper}>
-      <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+      <Formik
+        validationSchema={registerSchema}
+        initialValues={initialValues}
+        onSubmit={handleSubmit}
+      >
         <Form className={s.form}>
           <label className={s.label}>
             <span>Name</span>
             <Field className={s.input} name="name"></Field>
+            <ErrorMessage
+              name="name"
+              component="span"
+              className={s.error}
+            ></ErrorMessage>
           </label>
           <label className={s.label}>
             <span>Number</span>
